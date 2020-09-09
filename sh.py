aaea51af6743220650eea7bd9884c9ac614f1cad
drlazareff <62601222+drlazareff@users.noreply.github.com>
2020-09-09 16:51:23 +0300

    Update sh.py

--- sh.py
+++ sh.py
@@ -1,21 +1,27 @@
 import telebot
 import random as r
 import os
+import requests
+from bs4 import BeautifulSoup
 
-##from telebot import apihelper
-##apihelper.proxy = {'http':'http://195.201.137.246:1080'}
+##TO DO: переделать в конфиг
 
 bot = telebot.TeleBot('1399916936:AAHFUT2KDm5vDc6Spvzd4wDMa_Nh3BJiMSw')
 
-##directory= './Stickers/'
-directory= '/home/evs/telebot/Stickers'
+if os.name == 'nt' :
+	directory= './Stickers'
+else :
+	directory= directory= '/home/evs/telebot/Stickers'
+
+
 menuStickers = os.listdir(directory)
 menuHookah = ["шмаль и кокос","ёлки","ченить новое из дарка","дыня/арбуз","ягодный","фруктовый","десертный","танж с танжем и танжем", "Хватит курить!", "Для Андрейки","мёд с орешками","цитрусовый для Володьки","пушка страшная","марачино черри с чем-нить","ананас, кокос и абрикос","кисленький","вишневый","супернову для Лёшки"]
 menuDrinks = ["пиво","два бада","хватит бухать!!","bud", "гинесс", "стелла", "водички с ледиком","просто воды(", "ягер","козла","любое пиво","чаёк","ром с колой","чё-т я не в настроении"]
 menuGames = ["uno","контакт","молча бухаем","в молчанку","в крокодила","в шахматы с борисычем","время серьезное - не до игр!","в бутылочку!","в мафию","в телефоны залипайте, а?!"]
 
-Skills = "Пока я умею : \n/кальян - помогу выбрать паркур \n/пить - помогу с напитками \n/игра - подскажу во что поиграть"
+Skills = "Пока я умею : \n/кальян - помогу выбрать паркур \n/пить - помогу с напитками \n/игра - подскажу во что поиграть \n /юмор - подниму настроение"
 
+NoImages = ("/ЮМОР")
 
 ##print(directory+'/'+r.choice(menuStickers))
 
@@ -30,11 +36,46 @@ def choice(message):
 		newtext = r.choice(menuDrinks)
 	elif message.text.upper() == "/ИГРА" :
 		newtext = r.choice(menuGames)		
-	##elif message.text.upper() == "/HELP" :
-	##	newtext = Skills			
+	elif message.text.upper() == "/ЮМОР" :
+		newtext = getHumor()			
 	else :
 		newtext = ""
 	return newtext
+
+
+def getHumor():
+
+	try:
+		
+		HEADERS = {  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36', }
+		response = requests.get('https://bash.im/random',headers=HEADERS)
+		soup = BeautifulSoup(response.text, 'html.parser')
+		HumorList=[]
+		htmlParse = soup.find_all("div","quote__body")
+
+		for item in htmlParse:
+			HumorList.append(item)
+
+		humor = str(r.choice(htmlParse))
+		humor = replaceTags(humor)	
+		return humor	
+	except :
+		return "Ой, нишмагла ("	
+
+def replaceTags(text) :
+	text = text.replace("</br>","")
+	text = text.replace("<br/>","\n")
+	text = text.replace("<br>","\n")
+	text = text.replace('<div class="quote__body">',"")
+	text = text.replace("</div>","")
+	text = text.replace("&lt;","<")
+	text = text.replace("&gt;",">")
+
+	text = text.lstrip()
+
+	return text
+
+
 
 @bot.message_handler(commands=['start'])
 def get_start_messages(message):
@@ -50,10 +91,12 @@ def get_help_messages(message):
 def get_text_messages(message):
 	
 	resp = choice(message)
+
 	if resp != "" :
-		filename = directory+'/'+r.choice(menuStickers)
-		sti = open(filename, 'rb')
-		bot.send_sticker(message.chat.id, sti)
+		if NoImages.count(message.text.upper()) == 0 :
+			filename = directory+'/'+r.choice(menuStickers)
+			sti = open(filename, 'rb')
+			bot.send_sticker(message.chat.id, sti)
 
 		bot.send_message(message.chat.id,resp)
 		
